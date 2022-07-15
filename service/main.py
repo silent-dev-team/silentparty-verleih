447cfa0140d07386e3988b995a19c5f8d1b61787
theCVDH <c.cooly1111@web.de>
2022-07-15 20:06:48 +0200

    add Telegram notification

--- service/main.py
+++ service/main.py
@@ -1,9 +1,18 @@
+import os, telegram
 from fastapi import FastAPI
 
+TG_TOKEN = os.getenv('TG_TOKEN')
+TG_GROUP = int(os.getenv('TG_GROUP')) or None
 
 app = FastAPI()
-
+bot = telegram.Bot(TG_TOKEN)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/notify_auftrag")
+async def onAuftrag():
+    bot.send_message(text="Neue Anfrage über das Ausleihformular!", chat_id=TG_GROUP)
+    return {"message": "done"}
