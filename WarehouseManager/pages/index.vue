1f673634689f2a8cf982badfd3f416179bf9fad2
Jan <55839828+Piitschy@users.noreply.github.com>
2023-04-09 15:02:33 +0200

    add pinia

--- WarehouseManager/pages/index.vue
+++ WarehouseManager/pages/index.vue
@@ -23,7 +23,7 @@ const reset = () => {
         <v-btn class="mx-2" @click="reset">Reset</v-btn>
       </v-card>
       <v-card v-for="i in [1,2]" :key="i" class="mx-auto my-3 pa-5" width="300px">
-        <p>Shared Counter: {{ sharedCounter.count }}</p>
+        <p>SharedCounter{{ i }}: {{ sharedCounter.count }}</p>
         <v-btn class="mx-2" @click="sharedCounter.increment()">Shared +1</v-btn>
         <v-btn class="mx-2 my-1" @click="sharedCounter.reset()">Shared Reset</v-btn>
       </v-card>
