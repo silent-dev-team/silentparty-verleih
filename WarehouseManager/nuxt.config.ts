91fdb24b98983db151b82c94b78b4ecd162fbac8
Jan <55839828+Piitschy@users.noreply.github.com>
2023-04-09 14:08:17 +0200

    init WebApp "WarehouseManager"

---
+++ WarehouseManager/nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: true,
+  routeRules: {
+    '*/...': {ssr: false},
+  },
+  modules: [
+    '@nuxt/devtools',
+    "nuxt-directus"
+  ],
+  directus: {
+    url: 'http://cms.silentparty-hannover.de/',
+  },
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  }
+})
