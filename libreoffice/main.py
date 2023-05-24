79827a578f058ecc03c29999b3d221b71a9f7361
Jan <55839828+Piitschy@users.noreply.github.com>
2023-05-24 23:05:27 +0200

    add logger

--- libreoffice/main.py
+++ libreoffice/main.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI, UploadFile, logger, Response
-from fastapi.staticfiles import StaticFiles
+import logging
+from fastapi import FastAPI, UploadFile, Response
 from fastapi.responses import FileResponse
 from starlette.background import BackgroundTasks
-from dataclasses import dataclass
 import subprocess, os
-from datetime import datetime
-
-app = FastAPI()
 
 FOLDER = "./temp/"
+
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
 
 @app.get("/")
 async def root():
@@ -24,11 +24,12 @@ async def create_upload_file(file: UploadFile, bg_tasks: BackgroundTasks):
   source_path = f'{FOLDER}{name}.{ex}'
   target_path = f'{FOLDER}{name}.pdf'
 
-  with open(f'{FOLDER}{name}.{ex}', "wb") as buffer:
+  with open(source_path, "wb") as buffer:
     buffer.write(file.file.read())
 
-  proc = subprocess.run(["libreoffice","--headless","--convert-to","pdf",source_path,"--outdir",FOLDER+'pdf/'],capture_output=True)
-  logger.logger.log(0,proc.stderr)
+  proc = subprocess.run(["libreoffice","--headless","--convert-to","pdf",source_path,"--outdir",FOLDER],capture_output=True)
+  logger.info(proc.stdout)
+  logger.error(proc.stderr)
   
   bg_tasks.add_task(os.remove, source_path)
   bg_tasks.add_task(os.remove, target_path)
