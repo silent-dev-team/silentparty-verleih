bd82e2cec5704c46857a6a9a9e9aebc5b4a2e0e4
Jan <55839828+Piitschy@users.noreply.github.com>
2023-05-24 22:30:27 +0200

    finish odt->pdf

--- libreoffice/main.py
+++ libreoffice/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, logger, Response
 from fastapi.staticfiles import StaticFiles
-import subprocess
+from fastapi.responses import FileResponse
+from starlette.background import BackgroundTasks
+from dataclasses import dataclass
+import subprocess, os
+from datetime import datetime
 
 app = FastAPI()
 
@@ -10,13 +14,22 @@ FOLDER = "./temp/"
 async def root():
   return {"message": "Hello World"}
 
-@app.get("/ls")
-async def ls():
-  proc = subprocess.run(["ls"],capture_output=True)
-  return {"message": proc.stdout.decode("utf-8")}
+@app.post("/uploadfile", response_class=FileResponse)
+async def create_upload_file(file: UploadFile, bg_tasks: BackgroundTasks): 
+  try:
+    name, ex = file.filename.split('.')
+  except ValueError:
+    return Response('not a convertable file', status_code=400)
+  
+  source_path = f'{FOLDER}{name}.{ex}'
+  target_path = f'{FOLDER}{name}.pdf'
 
-@app.post("/uploadfile")
-async def create_upload_file(file: UploadFile):
-  with open(FOLDER+file.filename.strip().replace(' ','_'), "wb") as buffer:
+  with open(f'{FOLDER}{name}.{ex}', "wb") as buffer:
     buffer.write(file.file.read())
-  return {"filename": file.filename}
+
+  proc = subprocess.run(["libreoffice","--headless","--convert-to","pdf",source_path,"--outdir",FOLDER+'pdf/'],capture_output=True)
+  logger.logger.log(0,proc.stderr)
+  
+  bg_tasks.add_task(os.remove, source_path)
+  bg_tasks.add_task(os.remove, target_path)
+  return FileResponse(target_path, background=bg_tasks)
