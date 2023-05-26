2ce275a54652f12b41a2fb87543e1d86da32f350
Jan <55839828+Piitschy@users.noreply.github.com>
2023-05-26 16:45:11 +0200

    empty project

---
+++ form/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld msg="Hallo Christian" />
+    </v-main>
+  </v-app>
+</template>
